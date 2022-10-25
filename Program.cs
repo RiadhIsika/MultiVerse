e6bd8275352170dac908bac1d7390203f7bed6b6
Riadh SOUISSI <souissi.cnstn@gmail.com>
2022-10-25 12:36:36 +0200

    branche-C

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MultiVerse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World B!");
+            Console.WriteLine("Hello, World C!");
         }
     }
 }
